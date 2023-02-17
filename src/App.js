cdeb15726d308f24138da7e5e3a5e4104eb26044
Vivian A <vivian-mca@users.noreply.github.com>
2023-02-16 23:13:43 -0600

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Box } from "@mui/material";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import { ChannelDetail, Feed, Navbar, SearchFeed, VideoDetail } from "./components";
+
+const App = () => (
+  <BrowserRouter>
+    <Box sx={{ backgroundColor: "#000" }}>
+      <Navbar />
+      <Routes>
+        {/* A `Route` component is typically used to declare the routing configuration of a web application, specifying what components should be rendered for each specific URL path. 
+
+        What the properties mean:
+        - `path`: specifies URL path to match
+        - `element`: specifies the component to render when the path is matched
+        - `exact`: specifies that the patch should match exactly, i.e., it should not match any URL paths that include additional segments after the specified path
+        */}
+        <Route path="/" exact element={<Feed />} />
+        <Route path="/video/:id" element={<VideoDetail />} />
+        <Route path="/channel/:id" element={<ChannelDetail />} />
+        <Route path="/search/:searchTerm" element={<SearchFeed />} />
+      </Routes>
+    </Box>
+  </BrowserRouter>
+);
 
 export default App;
